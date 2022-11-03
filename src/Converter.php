27332899c82e2c7ad15fb49073952d1c28483a62
sun <invaderman@mail.ru>
2022-11-03 23:30:48 +0300

    Update to php 8.

--- src/Converter.php
+++ src/Converter.php
@@ -6,11 +6,9 @@ use Sun\Currency\Contracts\CourseContract;
 
 class Converter
 {
-    private CourseContract $course;
-
-    public function __construct(CourseContract $course)
-    {
-        $this->course = $course;
+    public function __construct(
+        private CourseContract $course,
+    ) {
     }
 
     public function convert(float $amount): float
